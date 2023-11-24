d1edafd20a0a0ca1edbb5a10c9da7a6fb271b644
Zainab <70613971+ZainabElSawy@users.noreply.github.com>
2023-11-24 20:44:18 +0200

    Delete chat_app directory

--- chat_app/lib/core/constant/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColor {
-  static const Color primaryColor = Color.fromRGBO(112, 62, 255, 1);
-  static Color backgroundColor = Colors.grey.shade100;
-  static Color grey = Colors.grey.shade400;
-  static Color white = Colors.white;
-}
